b6100d9a53fef22013eca0622d83b06fcb89e166
Sudar <sudar@sudarmuthu.com>
2011-08-12 18:13:07 +0530

    Moved constants to variables and added licence text

--- MissileLauncher.cpp
+++ MissileLauncher.cpp
@@ -1,36 +1,67 @@
+/**
+    MissileLauncher - Arduino library to work with USB MissileLauncher
+
+    http://sudarmuthu.com/arduino/missilelauncher
+
+    Requires v2.0 of USB Host Shield Library available at https://github.com/felis/USB_Host_Shield_2.0
+
+   Copyright 2011  Sudar Muthu  (email : sudar@sudarmuthu.com)
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <sudar@sudarmuthu.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer or coffee in return - Sudar
+ * ----------------------------------------------------------------------------
+ */
+
 #include "MissileLauncher.h"
 
 // constructor
 MissileLauncher::MissileLauncher() : USB() {
-    addr = 1; // Todo need to set it properly
+    addr = 1; // Todo need to change it if connected through a USB Hub
+    endPoint = 0;
+    bmReqType = 0x21;
+    bRequest = 0x09;
+    wValLo = 0x00;
+    wValHi = 0x02;
+    wInd = 0;
+    total = 1;
 }
 
+// Move Down
 void MissileLauncher::moveDown() {
     issueCommand(DOWN);
 }
 
+// Move Up
 void MissileLauncher::moveUp() {
     issueCommand(UP);
 }
 
+// Move Left
 void MissileLauncher::moveLeft() {
     issueCommand(LEFT);
 }
 
+// Move Right
 void MissileLauncher::moveRight() {
     issueCommand(RIGHT);
 }
 
+// fire
 void MissileLauncher::fire() {
     issueCommand(FIRE);
 }
 
+//stop
 void MissileLauncher::stop() {
     issueCommand(STOP);
 }
 
+// private helper function
 void MissileLauncher::issueCommand(MissileLauncherActions action) {
     byte data[1] = {action};
 
-    ctrlReq(addr, 0, 0x21, 0x09, 0x00, 0x02, 0, 1, 1, data, NULL); //TODO move to constants
+    ctrlReq(addr, endPoint, bmReqType, bRequest, wValLo, wValHi, wInd, total, sizeof(data)/sizeof(byte), data, NULL);
 }
--- MissileLauncher.h
+++ MissileLauncher.h
@@ -1,8 +1,26 @@
+/**
+    MissileLauncher - Arduino library to work with USB MissileLauncher
+
+    http://sudarmuthu.com/arduino/missilelauncher
+
+    Requires v2.0 of USB Host Shield Library available at https://github.com/felis/USB_Host_Shield_2.0
+
+   Copyright 2011  Sudar Muthu  (email : sudar@sudarmuthu.com)
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <sudar@sudarmuthu.com> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer or coffee in return - Sudar
+ * ----------------------------------------------------------------------------
+ */
+
 #include <Usb.h>
 
 #ifndef MissileLauncher_H
 #define MissileLauncher_H
 
+// Different possible acctions
 enum MissileLauncherActions {
     DOWN  = 1,  // down
     UP    = 2,    // up
@@ -13,11 +31,14 @@ enum MissileLauncherActions {
 };
 
 
-
+/**
+ * The core MissileLauncher class
+ */
 class MissileLauncher : public USB {
 
 public:
 
+    // constructor
     MissileLauncher();
 
     // USB related methods
@@ -35,7 +56,15 @@ private:
     void issueCommand(MissileLauncherActions);
 
     // variables
-    byte addr;
+    uint8_t addr;
+    uint8_t endPoint;
+    uint8_t bmReqType;
+    uint8_t bRequest;
+    uint8_t wValLo;
+    uint8_t wValHi;
+    uint16_t wInd;
+    uint16_t total;
+
 };
 
 #endif
